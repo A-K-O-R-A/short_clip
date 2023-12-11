a1eabe24c526d2edeea43014232503bbe616139a
AKORA <dragoncatm@gmail.com>
2023-12-11 17:36:21 +0100

    Remove imports

--- finni/src/main.rs
+++ finni/src/main.rs
@@ -2,8 +2,8 @@ use std::convert::Infallible;
 use std::net::SocketAddr;
 use std::path::PathBuf;
 
-use http_body_util::{Full, StreamBody};
-use hyper::body::{Body, Bytes};
+use http_body_util::Full;
+use hyper::body::Bytes;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Request, Response};
